bd0eb305a29b589046174eb1d5d68c6e29a754bf
howmon <awakejournal@gmail.com>
2026-05-04 13:56:55 -0700

    feat: add initial package.json for faunabrowsermcp server with dependencies and scripts

---
+++ faunabrowsermcp/preload.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('mcp', {
+  getStatus:  ()       => ipcRenderer.invoke('get-status'),
+  startRelay: ()       => ipcRenderer.invoke('start-relay'),
+  stopRelay:  ()       => ipcRenderer.invoke('stop-relay'),
+  copy:       txt      => ipcRenderer.invoke('copy', txt),
+  setLogin:   on       => ipcRenderer.invoke('set-login', on),
+  close:      ()       => ipcRenderer.invoke('close-popup'),
+  onLog:      cb       => ipcRenderer.on('log',    (_, d) => cb(d)),
+  onStatus:   cb       => ipcRenderer.on('status', (_, d) => cb(d))
+});
